01b5ee0c487eade65db5dd6defba0b7bbbb86b5d
verybadsoldier <vbs@springrts.de>
2024-10-09 21:27:49 +0200

    enable tar and delete

--- mc_backup.bat
+++ mc_backup.bat
@@ -11,8 +11,7 @@ REM Set the local folder where files will be downloaded
 set LOCAL_DIR=backups\%date%
 
 IF exist %LOCAL_DIR% (
-  echo Target directory "%LOCAL_DIR%" already exists. Will not be overwritten. Delete manually to create a new backup for today
-  exit /b 1
+  rmdir /s /q %LOCAL_DIR%
 )
 
 echo "Download to directory: %LOCAL_DIR%"
@@ -23,6 +22,6 @@ mkdir %LOCAL_DIR%
 echo wget -X %EXCLUDE_DIRS% --regex-type pcre --reject-regex %EXCLUDE_REGEX% --no-verbose --no-parent --recursive --no-directories --user=%USERNAME% --password=%PASSWORD% -x -P %LOCAL_DIR% %SERVER%
 wget -X %EXCLUDE_DIRS% --regex-type pcre --reject-regex %EXCLUDE_REGEX% --no-verbose --no-parent --recursive --no-directories --user=%USERNAME% --password=%PASSWORD% -x -P %LOCAL_DIR% %SERVER%
 
-REM tar.exe -a -c -f %LOCAL_DIR%.zip %LOCAL_DIR%
+tar.exe -a -c -f %LOCAL_DIR%.zip %LOCAL_DIR%
 
-REM rmdir /s /q %LOCAL_DIR%
+rmdir /s /q %LOCAL_DIR%
