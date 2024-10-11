3993febb2a325f9f2fb6fca50cbab607dd5547d7
verybadsoldier <vbs@springrts.de>
2024-10-11 09:56:54 +0200

    ask for server shutdown first, use relatives pathes in ZIP

--- mc_backup.bat
+++ mc_backup.bat
@@ -1,13 +1,18 @@
-@echo on
+@echo off
+
+set /p "stopped=Ist Server heruntergefahren? (YES eingeben, wenn ja): "
+
+IF NOT "%stopped%" == "YES" (
+  echo Dann bitte Server erst herunterfahren!
+  exit /b 1
+)
 
 call config.bat
-
 
 set EXCLUDE_DIRS=dynmap/web
 REM ignore all .bak files and all ZIP files in root folder
 set EXCLUDE_REGEX="ftp:\/\/([0-9]{1,3}\.){3}[0-9]{1,3}\/([^\/]*\.zip|.*\.bak)$"
 
-REM Set the local folder where files will be downloaded
 set LOCAL_DIR=backups\%date%
 
 IF exist %LOCAL_DIR% (
@@ -18,10 +23,8 @@ echo "Download to directory: %LOCAL_DIR%"
 
 mkdir %LOCAL_DIR%
 
-
-echo wget -X %EXCLUDE_DIRS% --regex-type pcre --reject-regex %EXCLUDE_REGEX% --no-verbose --no-parent --recursive --no-directories --user=%USERNAME% --password=%PASSWORD% -x -P %LOCAL_DIR% %SERVER%
 wget -X %EXCLUDE_DIRS% --regex-type pcre --reject-regex %EXCLUDE_REGEX% --no-verbose --no-parent --recursive --no-directories --user=%USERNAME% --password=%PASSWORD% -x -P %LOCAL_DIR% %SERVER%
 
-tar.exe -a -c -f %LOCAL_DIR%.zip %LOCAL_DIR%
+tar.exe -acf %LOCAL_DIR%.zip -C %LOCAL_DIR% *
 
 rmdir /s /q %LOCAL_DIR%
